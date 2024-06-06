018da0fe5e46f4236f7d2ad769665a5aab397ad7
zyufang <150459538+zyufang@users.noreply.github.com>
2024-06-06 02:50:08 +0000

    add

---
+++ bin/z_dart_test.dart
@@ -0,0 +1,5 @@
+import 'package:z_dart_test/z_dart_test.dart' as z_dart_test;
+
+void main(List<String> arguments) {
+  print('Hello world: ${z_dart_test.calculate()}!');
+}
---
+++ test/z_dart_test_test.dart
@@ -0,0 +1,8 @@
+import 'package:z_dart_test/z_dart_test.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
